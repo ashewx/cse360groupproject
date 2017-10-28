1db9ab7babdacb814bc4408253e018be54cbd560
Ben Shu <bgshu@asu.edu>
2017-10-28 11:11:23 -0700

    add new file for file reader

---
+++ src/cse360groupproject/ReadFromFile.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class ReadFromFile
+{
+	public  static void main(String[] args) throws Exception
+	{
+		File file = new File("C:\\Users\\bgshu\\Desktop\\test.txt");
+
+		BufferedReader br = new BufferedReader(new FileReader(file));
+
+		String st;
+		while ((st = br.readLine()) != null)
+		{
+			System.out.println(st);
+		}
+	}
+}
