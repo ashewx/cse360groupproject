0011b1179dce0c141efa7cb8da79baf88582b43f
Andrew Nguyen <ashewx@gmail.com>
2017-10-29 11:58:07 -0700

    Added a menu.

--- src/cse360groupproject/MainWindow.java
+++ src/cse360groupproject/MainWindow.java
@@ -3,10 +3,23 @@ package cse360groupproject;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JSplitPane;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class MainWindow {
 
-	private JFrame frame;
+	private JFrame frmTextAnalyzer;
 
 	/**
 	 * Launch the application.
@@ -16,7 +29,7 @@ public class MainWindow {
 			public void run() {
 				try {
 					MainWindow window = new MainWindow();
-					window.frame.setVisible(true);
+					window.frmTextAnalyzer.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +48,69 @@ public class MainWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 1146, 657);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTextAnalyzer = new JFrame();
+		frmTextAnalyzer.setTitle("Text Analyzer");
+		frmTextAnalyzer.setBounds(100, 100, 1146, 657);
+		frmTextAnalyzer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		GroupLayout groupLayout = new GroupLayout(frmTextAnalyzer.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(splitPane, GroupLayout.PREFERRED_SIZE, 1130, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addComponent(splitPane, GroupLayout.PREFERRED_SIZE, 618, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setEditable(false);
+		splitPane.setRightComponent(textArea);
+		
+		JPanel panel = new JPanel();
+		splitPane.setLeftComponent(panel);
+		
+		JButton btnLoadFile = new JButton("Load File");
+		
+		JButton btnFileHistory = new JButton("File History");
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnLoadFile)
+					.addGap(18)
+					.addComponent(btnFileHistory)
+					.addContainerGap(940, Short.MAX_VALUE))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnLoadFile)
+						.addComponent(btnFileHistory))
+					.addContainerGap(166, Short.MAX_VALUE))
+		);
+		panel.setLayout(gl_panel);
+		frmTextAnalyzer.getContentPane().setLayout(groupLayout);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmTextAnalyzer.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmLoadFile = new JMenuItem("Load File");
+		mnFile.add(mntmLoadFile);
+		
+		JMenuItem mntmFileHistory = new JMenuItem("File History");
+		mnFile.add(mntmFileHistory);
 	}
-
 }
