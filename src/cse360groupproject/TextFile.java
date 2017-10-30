e25350671e8494fe1b52e9c093ee1b6ba9001e12
Andrew Nguyen <ashewx@gmail.com>
2017-10-29 23:30:46 -0700

    Textfile class setup.

--- src/cse360groupproject/TextFile.java
+++ src/cse360groupproject/TextFile.java
@@ -1,7 +1,15 @@
 package cse360groupproject;
 
 public class TextFile {
-
+	private String name;
+	private String input;
+	private int numLines;
+	private int blankLn;
+	private int numSpaces;
+	private int numWords;
+	private int avgCharPerLn;
+	private int avgWrdLen;
+	
 	public TextFile() {
 		// TODO Auto-generated constructor stub
 	}
