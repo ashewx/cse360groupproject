a2795f88d60ee8e7e47b64f0f8279482da580dd6
Jordan Lewis <jalewi21@asu.edu>
2017-10-30 11:00:07 -0700

    Added line reader code

--- src/cse360groupproject/TextFile.java
+++ src/cse360groupproject/TextFile.java
@@ -1,5 +1,9 @@
 package cse360groupproject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TextFile {
 	private String name;
 	private String input;
@@ -10,8 +14,26 @@ public class TextFile {
 	private int avgCharPerLn;
 	private int avgWrdLen;
 	
-	public TextFile() {
+	public TextFile(String filename, String everythingInsideFile) {
 		// TODO Auto-generated constructor stub
 	}
+	
+	
+	public void LineCount(String filepath) throws FileNotFoundException
+	{
+		
+		
+		File aFile = new File("/Users/jordanlewis/Documents/workspace/FileReader/TestCases/Test_1.txt");
+		Scanner freader = new Scanner(aFile);
+		while (freader.hasNextLine()) 
+		{
+			  numLines++;
+			  freader.nextLine();
+		}
+		numLines = numLines -7;
+		System.out.println(numLines);
+		
+	}
+	
 
 }
