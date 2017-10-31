0249e8086815b0ba117af8b6724102b503047fa7
Andrew Nguyen <ashewx@gmail.com>
2017-10-30 18:04:42 -0700

    Blank lines counter.

--- src/cse360groupproject/TextFile.java
+++ src/cse360groupproject/TextFile.java
@@ -1,5 +1,9 @@
 package cse360groupproject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
 public class TextFile {
 	private String name;
 	private String input;
@@ -10,8 +14,87 @@ public class TextFile {
 	private int avgCharPerLn;
 	private int avgWrdLen;
 	
-	public TextFile() {
-		// TODO Auto-generated constructor stub
+	public TextFile(String name, String input) throws IOException {
+		this.name = name;
+		this.input = input;
+		this.blankLn = calcBlank(input);
+		
+	}
+	
+	public int calcBlank(String input) throws IOException {
+		BufferedReader br = new BufferedReader(new StringReader(input));
+		String line;
+		int empty = 0;
+		while ((line = br.readLine()) != null) {
+		  if (line.trim().isEmpty()) {
+		    empty++;
+		  }
+		}
+		return empty;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getInput() {
+		return input;
+	}
+
+	public int getNumLines() {
+		return numLines;
+	}
+
+	public int getBlankLn() {
+		return blankLn;
+	}
+
+	public int getNumSpaces() {
+		return numSpaces;
+	}
+
+	public int getNumWords() {
+		return numWords;
+	}
+
+	public int getAvgCharPerLn() {
+		return avgCharPerLn;
+	}
+
+	public int getAvgWrdLen() {
+		return avgWrdLen;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+
+	public void setNumLines(int numLines) {
+		this.numLines = numLines;
+	}
+
+	public void setBlankLn(int blankLn) {
+		this.blankLn = blankLn;
+	}
+
+	public void setNumSpaces(int numSpaces) {
+		this.numSpaces = numSpaces;
+	}
+
+	public void setNumWords(int numWords) {
+		this.numWords = numWords;
+	}
+
+	public void setAvgCharPerLn(int avgCharPerLn) {
+		this.avgCharPerLn = avgCharPerLn;
+	}
+
+	public void setAvgWrdLen(int avgWrdLen) {
+		this.avgWrdLen = avgWrdLen;
 	}
 
 }
